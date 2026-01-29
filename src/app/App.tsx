8f8bd98e1737b3d729b61e50f4c1c6bcd7ed2633
jmpizza <jespitiap@unal.edu.co>
2026-01-28 21:42:00 -0500

    Init repository

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen w-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Main Page of pokeapi test
+      </h1>
+    </div>
+  );
+}
