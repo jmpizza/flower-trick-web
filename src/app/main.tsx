a4821f20ee9fb16ed8265513d241a673ecf7eab4
jmpizza <jespitiap@unal.edu.co>
2026-02-13 07:56:51 -0500

    feat: implements initial page layout

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import {Navbar} from "../components/navbar.tsx"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"
+import "./index.css";
+import { Navbar } from "../components/Navbar.tsx";
+import { Footer } from "../components/Footer.tsx";
+import App from "./App.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Navbar />
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+        <Navbar />
+        <App />
+        <Footer />
+    </StrictMode>
+  </BrowserRouter>
+);
